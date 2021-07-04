b0347fccf3618595a0de4aab0eae0113b962f018
Jose Guillermo <jguillermo@outlook.com>
2021-07-04 14:58:43 -0500

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserResolver } from './app/user/user.resolver';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { UserResolver } from './app/user/user.resolver';
       debug: true,
     }),
     CqrsModule,
+    UserModule,
   ],
   providers: [UserResolver],
 })
