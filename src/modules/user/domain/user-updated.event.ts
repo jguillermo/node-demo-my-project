f76a4bef71100cfb037832465a1bbbbaa3f9b3cd
Jose Guillermo <jguillermo@outlook.com>
2021-08-14 00:44:50 -0500

    update events

--- src/modules/user/domain/user-updated.event.ts
+++ src/modules/user/domain/user-updated.event.ts
@@ -1,8 +1,16 @@
 import { EventBase } from 'base-ddd';
 
 export class UserUpdatedEvent extends EventBase {
-  constructor(public id: string, public name: string) {
+  constructor(private _id: string, private _name: string) {
     super();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
   }
 
   eventName(): string {
