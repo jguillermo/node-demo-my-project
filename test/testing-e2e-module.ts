d7e8784a05cd3b15ca7a497a3b4ee0ed6727d55b
Jose Guillermo <jguillermo@outlook.com>
2021-08-21 15:47:53 -0500

    refactor test e2e

--- test/testing-e2e-module.ts
+++ test/testing-e2e-module.ts
@@ -1,30 +1,31 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { UserRepository } from '../src/user/domain/user.repository';
 
-export interface CreateTestingInterface {
-  app: INestApplication;
-  userRepository: UserRepository;
-}
+export abstract class TestingE2eModule {
+  private _app: INestApplication;
+  private _moduleFixture: TestingModule;
 
-export class TestingE2eModule {
-  static async create(): Promise<CreateTestingInterface> {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  protected async init(): Promise<void> {
+    this._moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-    const app: INestApplication = moduleFixture.createNestApplication();
-    app.useGlobalPipes(
+    this._app = this._moduleFixture.createNestApplication();
+    this._app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
         transform: true,
         forbidNonWhitelisted: true,
       }),
     );
-    await app.init();
-    return {
-      app,
-      userRepository: moduleFixture.get<UserRepository>(UserRepository),
-    };
+    await this._app.init();
+  }
+
+  get app(): INestApplication {
+    return this._app;
+  }
+
+  get moduleFixture(): TestingModule {
+    return this._moduleFixture;
   }
 }
