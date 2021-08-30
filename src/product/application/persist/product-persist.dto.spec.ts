297ab4c6b46b31081675c1a432b3de123a05a402
Jose Guillermo <jguillermo@outlook.com>
2021-08-30 10:34:44 -0500

    feat: development product

--- src/product/application/persist/product-persist.dto.spec.ts
+++ src/product/application/persist/product-persist.dto.spec.ts
@@ -9,9 +9,9 @@ describe('ProductPersistDto', () => {
       dto.name = 'name';
       dto.code = 'e42ce453-ca22-5311-914d-76b8c4461e2b';
       dto.description = 'description';
-      dto.createAt = '2021-12-05';
+      dto.createAt = '2018-03-23';
       dto.price = 12.5;
-      dto.category = 'cat1';
+      dto.category = 'books';
       const errors = await validate(dto);
       expect(errors.length).toEqual(0);
     });
