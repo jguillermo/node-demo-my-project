9e154128cfa8ef7e67aa19d4b39542179a7abece
Jose Guillermo <jguillermo@outlook.com>
2022-04-24 10:17:47 -0500

    cucumber ts

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,27 @@
+// module.exports = {
+//   default: `--format-options '{"snippetInterface": "synchronous"}'`,
+// };
+
+// {
+//   "default": {
+//   "paths": [
+//     "features/**/*.feature"
+//   ],
+//       "parallel": 1,
+//       "format": [
+//     "html:cucumber-report.html"
+//   ]
+// }
+// }
+
+let common = [
+  'features/**/*.feature',
+  '--require-module ts-node/register',
+  '--require features/step-definitions/**/*.ts',
+  '--format html:cucumber-report.html',
+].join(' ');
+
 module.exports = {
-  default: `--format-options '{"snippetInterface": "synchronous"}'`,
+  default: common,
+  // More profiles can be added if desired
 };
