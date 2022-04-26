5a9be4cede4ea071bafb3c536e69d92b74faf7a6
Jose Guillermo <jguillermo@outlook.com>
2022-04-26 11:28:55 -0500

    refactor process string

--- features/step-definitions/tools/string-tools.ts
+++ features/step-definitions/tools/string-tools.ts
@@ -1,3 +1,5 @@
+import { ProcessValue } from './process-json/processValue';
+
 export function toJson(data: string): any {
   let dataJson = {};
   try {
@@ -13,28 +15,14 @@ export function processData(data: any): any {
     data.forEach((element, index) => {
       data[index] = processData(element);
     });
-  } else if (isObject(data)) {
+  } else if (data != null && data.constructor.name === 'Object') {
     const claves = Object.keys(data);
     for (let i = 0; i < claves.length; i++) {
       const clave = claves[i];
       data[clave] = processData(data[clave]);
     }
   } else {
-    data = processString(data);
+    data = ProcessValue.getInstance().process(data);
   }
   return data;
-}
-
-function processString(data: any) {
-  if (typeof data === 'string' || data instanceof String) {
-    const position = data.search(/^Date\([\d]{4}\-[\d]{2}\-[\d]{2}\)$/);
-    if (position === 0) {
-      data = new Date(data.substring(5, 15));
-    }
-  }
-  return data;
-}
-
-function isObject(obj) {
-  return obj != null && obj.constructor.name === 'Object';
 }
