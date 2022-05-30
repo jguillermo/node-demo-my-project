3f27f9b7429f9f51bbb891574b66e99834f53daa
Jose Guillermo <jguillermo@outlook.com>
2022-05-29 22:12:01 -0500

    solve dependencies

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,5 @@
 let common = [
-  //'test/features/**/*.feature',
-  'test/features/user/delete.feature',
+  'test/features/**/*.feature',
   '--require-module ts-node/register',
   '--require test/features/step-definitions/**/*.ts',
   '--format html:cucumber-report.html',
